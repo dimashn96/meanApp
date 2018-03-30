9c798d6becd0a75464a9d508632de1386efbb2ee
Dmitriy Shnitkovskiy <dm.shnit@gmail.com>
2018-03-30 13:05:46 +0300

    config server.ts

--- server.ts
+++ server.ts
@@ -12,20 +12,20 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 // Angular DIST output folder
-app.use(express.static(path.join(__dirname,'dist')));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 // API location
-app.use('/api',api);
+app.use('/api', api);
 
 // Send all other requests to the Angular app
-app.get('*',(req, res) => {
-  res.sendFile(path.join(__dirname,'dist/index.html'));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
 // Set port
 const port = process.env.PORT || '3000';
-app.set('port',port);
+app.set('port', port);
 
 // Server
 const server = http.createServer(app);
-server.listen(port,() => console.log(`Running on port: ${port}`));
+server.listen(port, () => console.log(`Running on port: ${port}`));
