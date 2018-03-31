3c7110db8847cf80aeee4577ca0754deb8f52038
Dmitriy Shnitkovskiy <dima.shn96@gmail.com>
2018-03-31 15:33:37 +0300

    edit config

--- server.ts
+++ server.ts
@@ -5,7 +5,7 @@ const http = require('http');
 const app = express();
 
 // API file for interacting with MongoDB
-const api = require('./server/routes/api');
+const api = require('./server/routes/api.ts');
 
 // Parsers
 app.use(bodyParser.json());
