5c3c3c4e8e5285382c648121c23807d690b4015b
Dmitriy Shnitkovskiy <dm.shnit@gmail.com>
2018-04-02 13:53:17 +0300

    refactor

--- server.ts
+++ server.ts
@@ -1,25 +1,25 @@
-const express = require('express');
+import * as express from 'express';
 const bodyParser = require('body-parser');
 const path = require('path');
 const http = require('http');
 const app = express();
 
 // API file for interacting with MongoDB
-const api = require('./server/routes/api');
+const api = require('../server/routes');
 
 // Parsers
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 // Angular DIST output folder
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, '../dist')));
 
 // API location
 app.use('/api', api);
 
 // Send all other requests to the Angular app
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/index.html'));
+  res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
 // Set port
